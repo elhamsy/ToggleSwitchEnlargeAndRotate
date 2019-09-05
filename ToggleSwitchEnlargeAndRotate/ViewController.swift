114e045634b3940b134e60c1f157f6a9d24a5591
elhamsy <elham@98chimp.com>
2019-09-05 14:18:05 -0700

    Initial commit

--- ToggleSwitchEnlargeAndRotate/ViewController.swift
+++ ToggleSwitchEnlargeAndRotate/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let toggleSwitch2 = UISwitch(frame: CGRect(x: 150, y: 300, width: 0, height: 0))
+
+    @IBOutlet weak var ToggleView: UIView!
+    @IBOutlet weak var lable: UILabel!
+    @IBAction func toggle(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            lable.text = "On"
+        }else{
+            lable.text = "Off"
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+ //       ToggleView.transform = CGAffineTransform(scaleX: 2, y: 2)
+ //   ToggleView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        ToggleView.transform =  CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//
+
+
+
+
     }
 
 
 }
 
+
+//class ViewController: UIViewController {
+//
+//    let switchSwift4 = UISwitch(frame:CGRect(x: 200, y: 300, width: 500, height: 500))
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        self.switchSwift4.isOn = true
+//        switchSwift4.setOn(true, animated: false)
+//        switchSwift4.addTarget(self, action: #selector(switchValueDidChange(_:)), for: .valueChanged)
+//
+//   //    switchSwift4.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+//        switchSwift4.transform = CGAffineTransform(scaleX: 2, y: 2)
+//        switchSwift4.transform = CGAffineTransform(rotationAngle: (CGFloat(Double.pi / 2)))
+//
+//        self.view!.addSubview(switchSwift4)
+//    }
+//
+//    @objc func switchValueDidChange(_ sender: UISwitch) {
+//        if switchSwift4.isOn == true {
+//            print("On")
+//        }
+//        else {
+//            print("Off")
+//        }
+//    }
+//}
